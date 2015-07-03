6a92a8476c366aadea826409b362a4cbf8a0e7bf
Walsh Costigan <walshcostigan@gmail.com>
2015-07-03 19:45:43 -0400

    Displays all LanguagesUser except for the current users

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -3,7 +3,7 @@ class ProfileController < ApplicationController
 	def home
 		#@user = User.find(params[:user_id])
 		@user = current_user
-		@matches = LanguagesUser.all
+		@matches = LanguagesUser.where("level <= '4'" && "language_id = '1'" && "user_id != '#{@user.id}'")
 	end
 
 	
