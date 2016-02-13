d4c321fad131f8c5bddc3978864421391af52ff4
Walsh Costigan <walshcostigan@gmail.com>
2016-02-13 15:30:34 -0500

    trying to pull from master

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,16 @@ class User < ActiveRecord::Base
   def mailboxer_email(object)
     email
   end
+
+  # def active_for_authentication? 
+  #   super && approved? 
+  # end 
+
+  # def inactive_message 
+  #   if !approved? 
+  #     :not_approved 
+  #   else 
+  #     super # Use whatever other message 
+  #   end 
+  # end
 end
