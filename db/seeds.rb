16c34c6d18fe6c08dfed5df6bab9417b8dced4ef
Walsh Costigan <walshcostigan@gmail.com>
2016-05-10 21:32:20 -0400

    fixing approved undefined method on heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,5 +14,5 @@ Role.create(name: "user")
 
 
 
-User.create(first_name: "God", last_name: "Costigan" email: => 'walshcostigan@gmail.com', location: "NYC", nationality: "American", bio: "Hello", admin: true, approved: true, role_id: 1, password: 'password', password_confirmation: 'password')
-User.create(first_name: "Walsh", last_name: "Costigan" email: => 'walsh@gmail.com', location: "NYC", nationality: "American", bio: "Hello", admin: true, approved: true, role_id: 0, password: 'password', password_confirmation: 'password')
+User.create(first_name: "God", last_name: "Costigan", email: 'walshcostigan@gmail.com', location: "NYC", nationality: "American", bio: "Hello", admin: true, approved: true, role_id: 1, password: 'password', password_confirmation: 'password')
+User.create(first_name: "Walsh", last_name: "Costigan", email: 'walsh@gmail.com', location: "NYC", nationality: "American", bio: "Hello", admin: true, approved: true, role_id: 0, password: 'password', password_confirmation: 'password')
