9e5c0f3fac43c519219b175dba8484760c4ce90a
Walsh Costigan <walshcostigan@gmail.com>
2016-05-14 12:58:38 -0400

    deleted requestor_topics migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160424223845) do
-
 
   create_table "languages", force: :cascade do |t|
     t.string   "language"
