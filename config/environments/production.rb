5b32814f01540dea6a38b2a5012918737f3376b5
Walsh Costigan <walshcostigan@gmail.com>
2016-06-15 13:23:25 -0400

    trying to fix font on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,8 +34,6 @@ Rails.application.configure do
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
-  config.assets.paths << Rails.root.join("app", "assets", "fonts")
-  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
