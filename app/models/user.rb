25924ac44cbb1670140b2356e40852edfe348e87
Walsh Costigan <walshcostigan@gmail.com>
2016-06-16 15:36:50 -0500

    approving users manually through edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-  validates_presence_of :first_name, :email, :native_language
+  validates_presence_of :first_name, :email
 
 	
   before_save :assign_role
